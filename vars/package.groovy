1d0f95de254c9c10b37ad54e9bcb9f27001330c3
Prawin132 <56030257+Prawin132@users.noreply.github.com>
2021-02-01 18:02:20 +0530

    Create package.groovy

---
+++ vars/package.groovy
@@ -0,0 +1,28 @@
+def call(body) {
+    def pipeline_params = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipeline_params
+    body()
+    
+    String PACKAGE_NAME = "drop"
+    if(pipeline_params.FIRST_NAME){
+        PACKAGE_NAME = pipeline_params.PACKAGE_NAME
+    }
+
+     
+    env.PACKAGE_NAME = PACKAGE_NAME
+    println "PACKAGE_NAME: ${env.PACKAGE_NAME}"
+   
+    
+    pipeline {
+    agent any
+
+    stages {
+        stage('print') {
+            steps {
+                echo "Name of the package ${PACKAGE_NAME}"
+            }
+        }
+    }
+}
+}
