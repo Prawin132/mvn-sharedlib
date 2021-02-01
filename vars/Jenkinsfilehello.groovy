7665b32b020c1696abcb057fbffafe8028dbec74
Prawin132 <56030257+Prawin132@users.noreply.github.com>
2021-02-01 16:53:31 +0530

    Update Jenkinsfilehello.groovy

--- vars/Jenkinsfilehello.groovy
+++ vars/Jenkinsfilehello.groovy
@@ -5,6 +5,13 @@ def call(body) {
     
     String FIRST_NAME = pipeline_params.FIRST_NAME
     String LAST_NAME = pipeline_params.LAST_NAME
+    if(pipeline_params.FIRST_NAME){
+        FIRST_NAME = "Ppraveen"
+        LAST_NAME = "Kkk"
+    }
+    
+    
+    
      
     env.FIRST_NAME = FIRST_NAME
     env.LAST_NAME = LAST_NAME
