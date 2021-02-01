4a353fd338325f30e9797d0d4edd80fe255834c7
Prawin132 <56030257+Prawin132@users.noreply.github.com>
2021-02-01 15:56:54 +0530

    Update Jenkinsfilehello.groovy

--- vars/Jenkinsfilehello.groovy
+++ vars/Jenkinsfilehello.groovy
@@ -1,10 +1,11 @@
 def call(Map pipelineParams) {
-    pipeline {
-    agent any
+    
         String FIRST_NAME = 'Praveen'
         if(pipeline_params.FIRST_NAME){
              FIRST_NAME=pipeline_params.FIRST_NAME
         }
+    pipeline {
+    agent any
     environment {
         
         LAST_NAME = 'K'
