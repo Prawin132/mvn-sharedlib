5a078aa8b4b114135edb21b8ade725624caef31b
Prawin132 <56030257+Prawin132@users.noreply.github.com>
2021-02-01 17:09:10 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -3,11 +3,13 @@ def call(body) {
     body.delegate = pipeline_params
     body()
     
-    String FIRST_NAME = pipeline_params.FIRST_NAME
-    String LAST_NAME = pipeline_params.LAST_NAME
-    if(!pipeline_params.FIRST_NAME){
-        FIRST_NAME = "Ppraveen"
-        LAST_NAME = "Kkk"
+    String FIRST_NAME = ""
+    String LAST_NAME = ""
+    if(pipeline_params.FIRST_NAME){
+        FIRST_NAME = pipeline_params.FIRST_NAME
+    }
+    if(pipeline_params.LAST_NAME){
+        LAST_NAME = pipeline_params.LAST_NAME
     }
     
     
