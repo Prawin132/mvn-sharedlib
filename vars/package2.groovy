bbf6173342af317f1e2de6957bdd511abbbc1c7a
Prawin132 <56030257+Prawin132@users.noreply.github.com>
2021-02-02 11:24:09 +0530

    Create package2.groovy

---
+++ vars/package2.groovy
@@ -0,0 +1,33 @@
+def call(body) {
+    def pipeline_params = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipeline_params
+    body()
+    
+    
+    pipeline {
+    agent any
+
+    stages {
+		stage('prepare') {
+            steps {
+               script {
+                   def props = readProperties file:"/var/lib/jenkins/workspace/${​​​​JOB_NAME}​​​​@libs/sharedlib/vars/${​​​​myprop}​​​​";
+                      env['PACKAGE_NAME'] = props['WAR_NAME'];
+
+                }
+            }
+        }
+        stage('build'){
+            steps{
+                sh 'mvn -B -DskipTests clean package'
+            }
+        }
+        stage('copy') {
+            steps {
+                sh 'mv webapp/target/*.war webapp/target/${PACKAGE_NAME}.war'
+            }
+        }
+    }
+}
+}
