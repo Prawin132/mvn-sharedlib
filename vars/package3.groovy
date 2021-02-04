7b260b4c22baffc1f219ee960f1aac6b141538c4
Prawin132 <56030257+Prawin132@users.noreply.github.com>
2021-02-04 15:41:46 +0530

    Create package3.groovy

---
+++ vars/package3.groovy
@@ -0,0 +1,35 @@
+def call(body) {
+    def pipeline_params = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipeline_params
+    body()
+	
+    //String myprop = " "
+	env.myprop = pipeline_params.myprop
+    
+    pipeline {
+    agent any
+
+    stages {
+		stage('prepare') {
+            steps {
+               script {
+                   def props = readProperties file:"/var/lib/jenkins/workspace/${JOB_NAME}@libs/mvn-sharedlib/vars/${myprop}";
+                      env['PACKAGE_NAME'] = props['WAR_NAME'];
+
+                }
+            }
+        }
+        stage('build'){
+            steps{
+                sh 'mvn -B -DskipTests clean package'
+            }
+        }
+        stage('copy') {
+            steps {
+                sh 'mv webapp/target/*.war webapp/target/${PACKAGE_NAME}.war'
+            }
+        }
+    }
+}
+}
